3161f3218d64690442bb3bda03e329223b6970d0
NKUNGA Jordan <jnkunga@hotmail.com>
2025-04-17 12:32:02 +0200

    ajustement formulaire

--- main.js
+++ main.js
@@ -178,25 +178,26 @@ const Home = {
       <div class="row justify-content-center">
         <div class="col-md-8">
           <form name="contact" method="POST" data-netlify="true" action="/contact">
-            <input type="hidden" name="form-name" value="contact" />
+  <input type="hidden" name="form-name" value="contact" />
+  
+  <div class="form-group">
+    <label for="name">Nom</label>
+    <input type="text" class="form-control" id="name" name="name" required />
+  </div>
 
-            <div class="form-group">
-              <label for="name">Nom</label>
-              <input type="text" class="form-control" id="name" name="name" required />
-            </div>
+  <div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="email" name="email" required />
+  </div>
 
-            <div class="form-group">
-              <label for="email">Email</label>
-              <input type="email" class="form-control" id="email" name="email" required />
-            </div>
+  <div class="form-group">
+    <label for="message">Message</label>
+    <textarea class="form-control" id="message" name="message" rows="4" required></textarea>
+  </div>
 
-            <div class="form-group">
-              <label for="message">Message</label>
-              <textarea class="form-control" id="message" name="message" rows="4" required></textarea>
-            </div>
+  <button type="submit" class="btn btn-outline-warning mt-3">Envoyer</button>
+</form>
 
-            <button type="submit" class="btn btn-outline-warning mt-3">Envoyer</button>
-          </form>
         </div>
       </div>
     </div>
